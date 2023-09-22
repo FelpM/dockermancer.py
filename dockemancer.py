cc2e7838ad4d06b99c41092667193a1e7f86c2d1
Felipe Madeira <109002732+FelpM@users.noreply.github.com>
2023-09-22 13:11:47 -0400

    Update dockemancer.py
    com api

--- dockemancer.py
+++ dockemancer.py
@@ -2,98 +2,134 @@
 #Suellen Guedes
 
 
+import requests
 import subprocess
 
-def executar_comando_docker(comando):
+def executar_comando_docker(api_url, comando_docker):
     try:
-        # Executar o comando Docker e capturar a saída
-        saida = subprocess.check_output(comando, universal_newlines=True, stderr=subprocess.STDOUT)
-        return saida
-    except subprocess.CalledProcessError as e:
-        # Lidar com erros, se houver algum
-        return f"Erro ao executar o comando: {e.output}"
+        # Enviar uma solicitação POST para a API Docker com o comando
+        response = comando_docker
+        print(f"Status da resposta: {response.status_code}")
+        print(f"Resposta: {response.text}")
         
-    
-def main ():
-    print("__<<SIMPLE DOCKER MAGEMENT & CONTROLLER>>__")
-    IP = input("DIgite o alvo IP:PORTA -> ")
-    while True:
-               
+        # Verificar se a solicitação foi bem-sucedida (código de status 200)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"Erro ao executar o comando: {response.text}"
+    except Exception as e:
+        return f"Erro ao executar o comando: {str(e)}"
 
+def main():
+    print("__<<SIMPLE DOCKER MAGEMENT & CONTROLLER>>__")
+    api_url = input("Digite a URL da API Docker:")  # Por exemplo, http://localhost:2375
+    while True:
         # Solicitar ao usuário que escolha o comando
         print("Escolha um comando:")
-        print("1. Listar Containers")
-        print("2. Listar imagens")
-        print("3. Listar Processos de um Container")
-        print("4. Capturar Logs de um Container")
-        print("5. Iniciar um Container")
-        print("6. Parar um Container")
-        print("7. Reiniciar um Container")
-        print("8. Matar um Container")
-        print("9. Remover um Container")
-        print("10. Remover uma Imagem")
-        print("11. Obter Informações do Sistema")
-        print("12. Obter Dados de Uso")
-        print("13. Obter Versão do Docker")
-        print("14. Obter um shell interativa")
-        print("15. Criar Container")
-        print("16. Baixar imagem")
+        print("1. List containers")
+        print("2. Create a container")
+        print("3. Inspect a container")
+        print("4. List processes running inside a container")
+        print("5. Get container logs")
+        print("6. Get changes on a container’s filesystem")
+        print("7. Get container stats based on resource usage")
+        print("8. Start a container")
+        print("9. Stop a container")
+        print("10. Restart a container")
+        print("11. Kill a container")
+        print("12. Rename a container")
+        print("13. Pause a container")
+        print("14. Unpause a container")
+        print("15. Remove a container")
+        print("16. Delete stopped containers")
+        print("17. List Images")
+        print("18. Inspect Images")
+        print("19. Remove an image")
+        print("20. Get system information")
+        print("21. Get data usage information")
+        print("22. Get data usage information")
+        print("23. Get version")
+        print("24. List network")
+        print("25. List config")
         opcao = input("Digite o número do comando desejado: ")
 
         # Definir o comando escolhido com base na opção do usuário
         if opcao == "1":
-            comando_docker = ["sudo", "docker", "-H", IP, "ps"]
+            comando_docker = requests.get(api_url + "/containers/json?all=1")
         elif opcao == "2":
-            comando_docker = ["sudo", "docker", "-H", IP, "images"]
+            nome_da_imagem = input("Digite o nome da imagem utilizada: ")
+            comando_docker = requests.post(api_url + "/containers/create", json={"Image": nome_da_imagem})
         elif opcao == "3":
-            container_id = input("Digite o ID do container para listar processos: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "top", container_id]
+            id = input("Digite o ID do container para inspecionar: ")
+            comando_docker = requests.get(api_url + f"/containers/{id}/json")
         elif opcao == "4":
-            container_id = input("Digite o ID do container para capturar logs: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "logs", container_id] 
+            id = input("Digite o ID do container para listar processos: ")
+            comando_docker = requests.get(api_url + f"/containers/{id}/top")
         elif opcao == "5":
-            container_id = input("Digite o ID do container para iniciar: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "start", container_id]
+            id = input("Digite o ID do container para capturar logs: ")
+            comando_docker = requests.get(api_url + f"/containers/{id}/logs")
         elif opcao == "6":
-            container_id = input("Digite o ID do container para parar: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "stop", container_id]
+            id = input("Digite o ID do container para obter mudanças no sistema de arquivos: ")
+            comando_docker = requests.get(api_url + f"/containers/{id}/changes")
         elif opcao == "7":
-            container_id = input("Digite o ID do container para reiniciar: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "restart", container_id]
+            id = input("Digite o ID do container para obter estatísticas: ")
+            comando_docker = requests.get(api_url + f"/containers/{id}/stats")
         elif opcao == "8":
-            container_id = input("Digite o ID do container para matar: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "kill", container_id]
+            id = input("Digite o ID do container para iniciar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/start")
         elif opcao == "9":
-            container_id = input("Digite o ID do container para remover: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "rm", container_id]
+            id = input("Digite o ID do container para parar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/stop")
         elif opcao == "10":
-            imagem_id = input("Digite o ID da imagem para remover: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "rmi", imagem_id]
+            id = input("Digite o ID do container para reiniciar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/restart")
         elif opcao == "11":
-            comando_docker = ["sudo", "docker", "-H", IP, "info"]
+            id = input("Digite o ID do container para matar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/kill")
         elif opcao == "12":
-            comando_docker = ["sudo", "docker", "-H", IP, "stats"]
+            id = input("Digite o ID do container para renomear: ")
+            novo_nome = input("Digite o novo nome: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/rename?name={novo_nome}")
         elif opcao == "13":
-            comando_docker = ["sudo", "docker", "-H", IP, "--version"]
+            id = input("Digite o ID do container para pausar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/pause")
         elif opcao == "14":
-            container_id = input("Digite o ID do container, para rodar: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "exec", "-it", container_id, "/bin/bash"]
-            processo = subprocess.Popen(comando_docker)
-            processo.communicate() 
+            id = input("Digite o ID do container para despausar: ")
+            comando_docker = requests.post(api_url + f"/containers/{id}/unpause")
         elif opcao == "15":
-            nome_imagem = input("Digite o nome da imagem para criar o container: ")
-            comando_docker = ["sudo", "docker", "-H", IP, "run", "-d", nome_imagem, "tail", "-f", "/dev/null"]
+            id = input("Digite o ID do container para remover: ")
+            comando_docker = requests.delete(api_url + f"/containers/{id}")
         elif opcao == "16":
-            nome_imagem = input("Digite o nome da imagem para baixar: ")
-            comando_docker = ["sudo", "docker", "pull", nome_imagem]
+            comando_docker = requests.post(api_url + "/containers/prune")
+        elif opcao == "17":
+            comando_docker = requests.get(api_url + "/images/json")
+        elif opcao == "18":
+            id = input("Digite o ID da imagem para inspecionar: ")
+            comando_docker = requests.get(api_url + f"/images/{id}/json")
+        elif opcao == "19":
+            id = input("Digite o ID da imagem para remover: ")
+            comando_docker = requests.delete(api_url + f"/images/{id}")
+        elif opcao == "20":
+            comando_docker = requests.get(api_url + "/info")
+        elif opcao == "21":
+            comando_docker = requests.get(api_url + "/system/df")
+        elif opcao == "22":
+            comando_docker = requests.get(api_url + "/system/df")
+        elif opcao == "23":
+            comando_docker = requests.get(api_url + "/version")
+        elif opcao == "24":
+            comando_docker = requests.get(api_url + "/networks")
+        elif opcao == "25":
+            comando_docker = requests.get(api_url + "/configs")
         else:
             print("Opção inválida.")
             exit()
 
         # Executar o comando escolhido
-        saida = executar_comando_docker(comando_docker)
+        saida = executar_comando_docker(api_url, comando_docker)
         print("\nSaída do comando:")
         print(saida)
-
+ 
 if __name__ == "__main__":
     main()
+
